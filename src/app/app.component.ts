17277ee5ecee9231595e411e628ffa5b01469ff4
Ali Abdelbaky <ali.abdelbaky2000@gmail.com>
2021-11-26 17:50:08 +0200

    development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'IX-login';
+  options: AnimationOptions
+  LoginForm: FormGroup | any;
+  constructor(private fb: FormBuilder) {
+    this.options = {
+      path:'assets/working-man.json'
+    }
+  }
+  ngOnInit() {
+    this.LoginForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      phone: ['', [Validators.minLength(8), Validators.maxLength(11)]]
+    });
+  }
+  submit() {
+    alert('watch the result in console')
+    console.log(this.LoginForm.value)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+//- lottie animation
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web'
+export function playerFactory() {
+  return player
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LottieModule.forRoot({ player: playerFactory })
   ],
   providers: [],
   bootstrap: [AppComponent]
